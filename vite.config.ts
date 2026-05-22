f0805a374829a16caec7bb83267c71a234d6a3fb
John Happel <johnhappel79@gmail.com>
2026-05-21 23:19:41 -0700

    Add launcher-based PWA icon set

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,12 @@ export default defineConfig({
     react(),
     VitePWA({
       includeAssets: [
-        "assets/apple-touch-icon.svg",
-        "assets/icon.svg",
-        "assets/maskable-icon.svg",
+        "assets/apple-touch-icon-180.png",
+        "assets/favicon-32.png",
+        "assets/icon-192.png",
+        "assets/icon-512.png",
+        "assets/maskable-icon-192.png",
+        "assets/maskable-icon-512.png",
       ],
       manifest: {
         background_color: "#ffffff",
@@ -18,15 +21,27 @@ export default defineConfig({
         icons: [
           {
             purpose: "any",
+            sizes: "192x192",
+            src: "/assets/icon-192.png",
+            type: "image/png",
+          },
+          {
+            purpose: "any",
             sizes: "512x512",
-            src: "/assets/icon.svg",
-            type: "image/svg+xml",
+            src: "/assets/icon-512.png",
+            type: "image/png",
+          },
+          {
+            purpose: "maskable",
+            sizes: "192x192",
+            src: "/assets/maskable-icon-192.png",
+            type: "image/png",
           },
           {
             purpose: "maskable",
             sizes: "512x512",
-            src: "/assets/maskable-icon.svg",
-            type: "image/svg+xml",
+            src: "/assets/maskable-icon-512.png",
+            type: "image/png",
           },
         ],
         name: "Westminster Catechizer",
